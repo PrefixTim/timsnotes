f85d0141ab6e0bf683989bfe16f7b682631e34ca
Timofey Malko <developer.of.gift@gmail.com>
2023-04-29 14:44:54 -0700

    add simple fakedb

--- tmn_api/src/main.rs
+++ tmn_api/src/main.rs
@@ -1,9 +1,15 @@
 #[macro_use] extern crate rocket;
 
-use rocket::serde::json::Json;
-use uuid::Uuid;
-use tmn_base::Tag;
+use std::sync::Arc;
 
+use rocket::{serde::json::Json, State};
+use uuid::Uuid;
+use tmn_base::{tag::Tag, content::Thought};
+
+struct FakeDb {
+    tags: Vec<Tag>,
+    thoughts: Vec<Thought>
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -11,12 +17,14 @@ fn index() -> &'static str {
 }
 
 #[get("/tag")]
-fn get_tag() -> Json<Tag> {
-    Json(Tag::new("title".to_owned()))
+fn get_tag(state: &State<Arc<FakeDb>>) -> Json<Tag> {
+    Json(Tag::new(Uuid::new_v4().into(), "title".to_owned()))
 }
-
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, get_tag])
+    let fdb =Arc::new(FakeDb{tags: Vec::new(), thoughts: Vec::new()});
+    rocket::build()
+    .mount("/", routes![index, get_tag, tag_title])
+    .manage(fdb)
 }
